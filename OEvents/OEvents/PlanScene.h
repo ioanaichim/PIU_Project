81cde45e8bd05a2abc38a7bdf04844ae027bdd8f
claudia-radion <claudia.radion@student.tuiasi.ro>
2022-01-14 09:30:52 +0200

    Comentarii cod

--- OEvents/OEvents/PlanScene.h
+++ OEvents/OEvents/PlanScene.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Element.h"
 
 #include <QGraphicsScene>
@@ -6,48 +6,54 @@
 //fereastra in care se construieste planificarea
 class PlanScene : public QGraphicsScene
 {
+    /* Macro non-standard pentru adăugarea unui comportament dinamic al claselor/obiectelor */
+    /* Sintaxa pentru semnale si sloturi nu respectă standardele C++, 
+    este prelucrată de MOC, de aceea apare macro-ul Q_OBJECT */
     Q_OBJECT
 
 public:
+    /* Proprietati publice */
     enum Mode { InsertItem, MoveItem };
 
+    /* Costructor */
     explicit PlanScene(QMenu* itemMenu, QObject* parent = nullptr);
 
+    /* Gettere si Settere pentru proprietatile private */
     QColor itemColor() const { return myItemColor; }
     QColor lineColor() const { return myLineColor; }
+
+    /* Metode publice */
     void setItemColor(const QColor& color);
-
     void setLineColor(const QColor& color);
-
     bool hasChanges();
 
 public slots:
+    /* Metode de tip slots */
     void setMode(Mode mode);
     void setItemShape(Element::Shape shape);
     void setItemType(Element::ElementType type);
 
-
 signals:
+    /* Metode de tip signals */
     void itemInserted(Element* item);
     void itemSelected(QGraphicsItem* item);
 
 
 protected:
+    /* Metode protected */
     void mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent) override;
     void mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent* mouseEvent) override;
 
 private:
-
+    /* Metode private */
     QMenu* myItemMenu;
     Mode myMode;
     Element::Shape myItemShape;
     Element::ElementType myItemType;
     
-    //punctul in care plasez item-ul
-    QPointF startPoint;
-    //item-ul poate fi colorat diferit si conturul lui de asemenea///cel mai probabil va fi pus in proprietatile elementului
-    QColor myItemColor;
+    QPointF startPoint;  //punctul in care plasez item-ul
+    QColor myItemColor;  //item-ul poate fi colorat diferit si conturul lui de asemenea
     QColor myLineColor;
 };
 
